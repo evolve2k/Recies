f9f5216da6ecee777c453b9a9416e325b60b046d
Richie Khoo <evolve2k@gmail.com>
2011-11-01 21:15:54 +1100

    Initial commit - Rails 3.1 project. Scaffolding for Recipe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recies::Application.config.secret_token = 'd992517a3dbdc8ec14a7590635227a42d51d044dff5a80c2cb4f5a43165403c4e03ab93ff1682dff3bfe4692a16c5b209dcc67acdba889be7874bde129808d23'
